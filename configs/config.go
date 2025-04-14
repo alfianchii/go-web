1e228e73d8fe584697c9ee51f787923ea8352041
alfianchii <pianaee379@gmail.com>
2025-04-14 17:55:04 +0700

    feat: `InitENV` func

--- configs/config.go
+++ configs/config.go
@@ -30,6 +30,26 @@ var (
 	TokenDuration = 1*time.Hour
 )
 
+func InitENV() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return &Config{
+		AppName: GetENV("APP_NAME"),
+		AppURL: GetENV("APP_URL"),
+		AppPort: GetENV("APP_PORT"),
+		DBDriver: GetENV("DB_DRIVER"),
+		DBHost: GetENV("DB_HOST"),
+		DBPort: GetENV("DB_PORT"),
+		DBName: GetENV("DB_DATABASE"),
+		DBUser: GetENV("DB_USERNAME"),
+		DBPass: GetENV("DB_PASSWORD"),
+		JWTSecret: GetENV("JWT_SECRET"),
+	}
+}
+
 func GetENV(key string) string {
 	dotEnv, err := godotenv.Read()
 	if err != nil {
