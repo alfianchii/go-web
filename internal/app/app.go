a4ac80d43ae8de97c352e4c02449499b14ee2229
alfianchii <pianaee379@gmail.com>
2025-04-14 18:06:17 +0700

    feat: app init

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,20 @@
 package app
+
+import (
+	"go-boilerplate/configs"
+	"go-boilerplate/internal/database"
+)
+
+type App struct {
+	DB *database.DB
+	// Repo, hdlr, svc
+}
+
+func InitApp() *App {
+	cfg := configs.InitENV()
+	db := database.InitDB(cfg)
+
+	return &App{
+		DB: db,
+	}
+}
